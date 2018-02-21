653182cf55a94fdfa7f4b65b134be3c4a6ed05a5
= <mateusz.prusaczyk@gmail.com>
2018-02-21 14:58:40 +0100

    more mapping for terms

--- src/api/mapEntryResponse.js
+++ src/api/mapEntryResponse.js
@@ -1,3 +1,6 @@
+import isString from 'lodash/isString';
+import isUndefined from 'lodash/isUndefined';
+
 const getDefinition = (entry) => {
     if (entry.definition && entry.definition[0] && entry.definition[0].dt) {
         const dt = entry.definition[0].dt[0];
@@ -24,24 +27,64 @@ const getDefinition = (entry) => {
 }
 
 
-const mapResponse = (entry) => {
-    return {
-        headword: entry.hw,
-        pronunciation: entry.pr,
-        definition: entry.def,
-        definedRunOn: entry.dro,
-        inflection: entry.in,
-        functionalLabel: entry.fl,
-        self: entry.$
+const mapSelf = (self) => {
+    return self.id;
+}
+
+const mapHeadword = (headword) => {
+    if (isString(headword[0])) {
+        //no highlight
+        return {
+            self: headword[0],
+            highlight: false
+        }
+    } else {
+        //highlight
+        return {
+            self: headword[0]._,
+            highlight: true
+        }
     }
 }
 
+const mapFunctionalLabel = (functionalLabel) => {
+    const label = functionalLabel[0];
+
+    return {
+        name: label,
+        isNoun: label === 'noun',
+        isVerb: label === 'verb',
+        isAdjective: label === 'adjective',
+        isAdverb: label === 'adverb',
+        isPreposition: label === 'preposition'
+    }
+}
+
+const mapPronounciation = (pronunciation) => {
+    if (isUndefined(pronunciation)) {
+        return null;
+    } else {
+        return pronunciation[0];
+    }
+}
+
+const mapResponse = (entry) => {
+    return {
+        self: mapSelf(entry.$),
+        headword: mapHeadword(entry.hw),
+        functionalLabel: mapFunctionalLabel(entry.fl),
+        pronunciation: mapPronounciation(entry.pr),
+        definition: entry.def,
+        definedRunOn: entry.dro,
+        inflection: entry.in
+    }
+}
 
 const mapModel = (entry) => {
     return {
         pronunciation: entry.pronunciation ? entry.pronunciation[0] : '',
-        partOfSpeech: entry.functionalLabel ? entry.functionalLabel[0]: '',
-        term: entry.self ? entry.self.id : '',
+        partOfSpeech: entry.functionalLabel,
+        term: entry.self ? entry.self : '',
         definition: getDefinition(entry)
     }
 }
@@ -49,6 +92,9 @@ const mapModel = (entry) => {
 
 export default (data) => {
     if (data.entry_list.entry) {
+        console.log('raw', data.entry_list.entry);
+        console.log( data.entry_list.entry
+            .map(mapResponse));
         return data.entry_list.entry
             .map(mapResponse)
             .map(mapModel);
